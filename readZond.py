2f08bd5940910cc168d69a716248dff4c54b2e7e
Michael Christoffersen <mchristo28@gmail.com>
2023-03-05 14:44:16 -0900

    Dictionary update

--- readZond.py
+++ readZond.py
@@ -149,20 +149,19 @@ def read(file, loglevel="INFO"):
     dDist = np.sqrt(dx**2 + dy**2 + dz**2)
     dist = np.concatenate(([0], np.cumsum(dDist)))
         
-    dataDict = {
-        "bscan": bscan,
-        "lon": lon,
-        "lat": lat,
-        "hgt": hgt,
-        "agl": agl,
-        "dist": dist,
-        "speed": speed,
-        "rtkMask": rtkMask,
-        "altMask": aglMask,
-        "speedMask": speedMask,
-        "time": time,
-        "dt": dt,
-    }
+dataDict = {
+    "bscan": bscan,           # The BScan or radargram, data acquired by the GPR (size: mxn)
+    "lon": lon,               # Longitude of the UAS (WGS84) (size: n)
+    "lat": lat,               # Latitude of the UAS (WGS84) (size: n)
+    "hgt": hgt,               # Height of the UAS about the WGS84 ellipsoid (size: n)
+    "agl": agl,               # Height of the UAS above ground (size: n)
+    "speed": speed,           # Speed of the UAS (m/s) (size: n)
+    "rtkMask": rtkMask,       # Mask for lon, lat, and hgt fields denoting which entries come from RTK (True == RTK) (size: n)
+    "altMask": aglMask,       # Mask for agl field denoting which entries come from laser altimeter (True == altimeter) (size: n)
+    "speedMask": speedMask,   # Mask for speed field denoting which entries are not nan (True == not nan) (size: n)
+    "time": time,             # Time of the acquisition of each trace (size: n)
+    "dt": dt,                 # Fast time sampling interval of the GPR (size: 1)
+}
     
     log.info("Zond Aero 500 file read complete")
     return dataDict
